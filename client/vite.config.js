d6e03051b676da69ababadf9bc8dcf0c91eb9c1e
sitebuilderr <63208180+sitebuilderr@users.noreply.github.com>
2025-07-26 11:33:27 -0400

    Add files via upload

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,9 @@
-// /client/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ['esbuild'], // ✅ add this line to avoid internalizing esbuild
-    },
-  },
+    outDir: 'dist'
+  }
 });
-
